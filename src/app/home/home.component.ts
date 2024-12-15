1a91b684a7265968544daadd23114739c5d9d1e4
OmiHernandez <hernandez.romo.n14@gmail.com>
2024-12-15 00:32:13 -0600

    cambios por aqui y por alla

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CategoriesComponent } from "../categories/categories.component";
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CategoriesComponent],
+  imports: [CategoriesComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  get username(): string | null {
+    return localStorage.getItem('username');
+  }
 }
